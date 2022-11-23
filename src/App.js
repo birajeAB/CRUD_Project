6ed7dff33192d53f66f030c633e411accf6dcf87
birajeajinkya <birajeajinkya7@gmail.com>
2022-11-23 19:16:14 +0530

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Contact from './Components/Pages/Contact';
+import About from './Components/Pages/About';
+import Home from './Components/Pages/Home';
+import Navbar from './Components/Layout/Navbar';
+import { Route,Router,Routes} from 'react-router-dom'
 
-function App() {
+
+const App = () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+
+
+<Navbar/>
+<Routes> 
+
+<Route  exact path="/" element={<Home/>}/>
+<Route    path="/about" element={<About/>}/>
+
+</Routes>
+
+
+    
+ 
+    </>
   );
 }
 
